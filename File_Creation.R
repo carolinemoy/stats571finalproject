c3e09e1e458fc8a2f2d8075f8971889889fe5de8
Caroline Moy <cmoy10@icloud.com>
2026-04-01 17:53:32 -0400

    Analyzing Differences Amongst Complete and Non-complete Cases

--- File_Creation.R
+++ File_Creation.R
@@ -38,3 +38,24 @@ clean_df <- clean_df %>%
          mean_bp = diastolic_avg + (1/3) * (systolic_avg - diastolic_avg))
 
 write.csv(clean_df, file = "/Users/carolinemoy/stats571finalproj/Data/clean_df.csv", row.names = FALSE)
+
+# Encoding all of clean_df and replacing
+clean_df <- clean_df %>% 
+  mutate(gender = ifelse(gender == "Female", 1, 0), # Female is 1, Male 0
+         Mexican = ifelse(race_ethnicity_detailed == "Mexican American", 1, 0), 
+         Asian = ifelse(race_ethnicity_detailed == "Non-Hispanic Asian", 1, 0),
+         Black = ifelse(race_ethnicity_detailed == "Non-Hispanic Black", 1, 0), 
+         White = ifelse(race_ethnicity_detailed == "Non-Hispanic White", 1, 0), 
+         Hispanic = ifelse(race_ethnicity_detailed == "Other Hispanic", 1, 0), 
+         Other = ifelse(race_ethnicity_detailed == "Other Race", 1, 0), 
+         Less_9th_grade = ifelse(education_level == "<9th grade", 1, 0), 
+         Some_High_School = ifelse(education_level == "9-11th grade", 1, 0), 
+         High_School_Degree = ifelse(education_level == "High school/GED", 1, 0), 
+         Some_College = ifelse(education_level == "Some college/AA", 1, 0), 
+         College_Grad_or_More = ifelse(education_level == "College graduate or above", 1, 0), 
+         Divorced = ifelse(marital_status == "Divorced", 1, 0), 
+         Married = ifelse(marital_status == "Married", 1, 0), 
+         Widowed = ifelse(marital_status == "Widowed", 1, 0)) 
+
+
+write.csv(clean_df, file = "/Users/carolinemoy/stats571finalproj/Data/clean_df.csv", row.names = FALSE)
